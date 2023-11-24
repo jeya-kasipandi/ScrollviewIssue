cb20568b799d58d2fe62390cdadc0750b4587951
jeya-kasipandi <92785406+jeya-kasipandi@users.noreply.github.com>
2023-11-24 15:43:04 +0530

    Add files via upload

---
+++ scrollviewissue/ScrollViewIssue/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace ScrollViewIssue;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		
+	}
+
+    void Button_Clicked(System.Object sender, System.EventArgs e)
+    {
+        count++;
+        horizontalLayout.Children.Add(new Label() { Text = "New Label" + count });
+
+    }
+   
+}
+
+
